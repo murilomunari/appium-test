960c9afe54f6221848e4b12b6c7300850a6b02c7
Murilo <murilomunari441@gmail.com>
2025-05-20 16:15:49 -0300

    Primeiras configs

---
+++ src/test/java/com/test/screen/ScreenLogin.java
@@ -0,0 +1,14 @@
+package com.test.screen;
+
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.remote.RemoteWebElement;
+
+public class ScreenLogin extends BaseScreen {
+
+    @AndroidFindBy(id = "login_username")
+    private RemoteWebElement fieldEmail;
+
+    public void writeEmail(){
+        fieldEmail.sendKeys("qazando@gmail.com");
+    }
+}
